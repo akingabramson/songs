1e02b8a35a49f79bb8cab119ceaa8f512a0e596f
Asher King Abramson <a.kingabramson@gmail.com>
2013-09-29 11:18:49 -0700

    updating vote error message

--- app/assets/javascripts/songshow.js
+++ app/assets/javascripts/songshow.js
@@ -110,7 +110,7 @@ createVote = function(upvoted, guessId, upvoteRequest, voteButton) {
 			resetVoteEventHandlers();
 		},
 		error: function() {
-			fireModal("Voting error.  What is this, the 2000 presidential campaign?")
+			fireModal("Didn't log vote.  Make sure you're logged in.")
 		}
 	})
 }
@@ -132,7 +132,7 @@ editVote = function(voteId, voteButton, upvoteRequest, guessId) {
 			$(".vote").on("click", checkVote)
 		},
 		error: function() {
-			fireModal("Voting error.  What is this, the 2000 presidential election?  Make sure you're logged in.")
+			fireModal("Couldn't vote. Make sure you're logged in.")
 		}
 	})
 }
